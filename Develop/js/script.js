54d0d275b5d92ae7f0104a63bc2a984a0b31d194
Nick <nprice0502@yahoo.com>
2023-05-09 17:19:10 -0400

    added variables in Js

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,3 +1,15 @@
 var weatherAPIKey = "10c964033c59d83e5d9d1b2413eb1de5";
-var city ="";
+var apiUrl = "https://api.openweathermap.org/data/2.5/";
+
+
+// Get DOM elements for search input, search button, current weather container, and forecast container
+var searchInput = document.getElementById("search-input");
+var searchBtn = document.querySelector("form button");
+var currentWeatherContainer = document.querySelector(".current-weather");
+var forecastContainer = document.querySelector(".forecast");
+
+
+// Define arrays for search history and city names
+let searchHistory = [];
+let cityNames = [];
 
