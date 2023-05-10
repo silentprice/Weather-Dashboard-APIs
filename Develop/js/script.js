d61196c440763c64d75a3564be44b708f3f1b8ee
Nick <nprice0502@yahoo.com>
2023-05-09 22:24:14 -0400

    updated the for loop to updated the 5 day forcast

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,15 +1,18 @@
-var weatherAPIKey = "10c964033c59d83e5d9d1b2413eb1de5";
-var apiUrl = `https://api.openweathermap.org/data/2.5/`;
+const weatherAPIKey = "10c964033c59d83e5d9d1b2413eb1de5";
+const apiUrl = `https://api.openweathermap.org/data/2.5/`;
+// const dayjs = dayjs();
 
 // Get DOM elements for search input, search button, current weather container, and forecast container
-var searchInput = document.getElementById("search-input");
-var searchBtn = document.querySelector("#search-btn");
-var currentWeatherContainer = document.querySelector(".current-weather");
-var forecastContainer = document.querySelector(".forecast");
+const searchInput = document.getElementById("search-input");
+const searchBtn = document.querySelector("#search-btn");
+const currentWeatherContainer = document.querySelector(".current-weather");
+const forecastContainer = document.querySelector(".forecast");
+
+const currentCity = document.getElementById("city-name");
+const weatherIcon = document.querySelector(".weather-icon");
 
 // Define arrays for search history and city names
 let searchHistory = [];
-// let cityNames = [];
 
 searchBtn.addEventListener("click", function () {
   let city = searchInput.value;
@@ -25,10 +28,64 @@ function getWeatherData(cityName) {
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
+
+        const lat = data.coord.lat;
+        const lon = data.coord.lon;
+        getFiveDayWeather(lat, lon);
+
+        currentCity.textContent = data.name;
+        weatherIcon.setAttribute(
+          "src",
+          `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+        );
+
+        document.getElementById("date").textContent =
+          dayjs().format("dddd MMMM DD, YYYY");
+      });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function getFiveDayWeather(lat, lon) {
+  try {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherAPIKey}&units=imperial`
+    )
+      .then((res) => res.json())
+      .then((fiveDayData) => {
+        console.log(fiveDayData);
         
-        var lat = data.coord.lat
-        var lon = data.coord.lon
-        
+        let dayIndex = 1
+        for (let i = 0; i < fiveDayData.list.length; i++) {
+            if (i % 8 === 0) {
+
+                let fiveDayDate =document.createElement("p");
+                fiveDayTemp.setAttribute("class", "five-day-date");
+                fiveDayDate = dayjs().add(dayIndex, "days").format('dddd');
+                forecastContainer.append(fiveDayDate);
+                dayIndex++
+
+                let fiveDayTemp = document.createElement('p');
+                fiveDayTemp.setAttribute("class", "five-day-temp");
+                fiveDayTemp.textContent = fiveDayData.list[i].main.temp_max
+                forecastContainer.append(fiveDayTemp);
+
+                let fiveDayHumid = document.createElement('p');
+                fiveDayHumid.setAttribute("class", "five-day-humid");
+                fiveDayHumid.textContent = fiveDayData.list[i].main.humidity
+                forecastContainer.append(fiveDayHumid);
+
+                let fiveDayWind = document.createElement('p');
+                fiveDayWind.setAttribute("class", "five-day-wind");
+                fiveDayWind.textContent = fiveDayData.list[i].wind.speed
+                forecastContainer.append(fiveDayWind);
+
+
+
+            }
+        }
+
       });
   } catch (error) {
     console.log(error);
